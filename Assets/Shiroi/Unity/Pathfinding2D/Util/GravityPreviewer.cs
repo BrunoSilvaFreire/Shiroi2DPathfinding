1b37b895d03c474c530a22bc5b7667a06ec810a8
DDEVIL\bruno <brunorbsf@gmail.com>
2017-04-19 20:13:07 -0300

    - Created a new class TestEntity.cs for entity testing. - GravityPreviewer.cs no longer implements IGroundEntity, you now need to pass a IGroundEntity to it instead.

--- Assets/Shiroi/Unity/Pathfinding2D/Util/GravityPreviewer.cs
+++ Assets/Shiroi/Unity/Pathfinding2D/Util/GravityPreviewer.cs
@@ -3,57 +3,26 @@ using Vexe.Runtime.Types;
 
 namespace Shiroi.Unity.Pathfinding2D.Util {
     [RequireComponent(typeof(Rigidbody2D))]
-    public class GravityPreviewer : BaseBehaviour, IGroundEntity {
-        [Show]
-        public Rigidbody2D RigidBody {
-            get;
-            private set;
-        }
-
-        public GravityPreviewer() {
-            MaxSpeed = 2f;
-            JumpHeight = 5F;
-        }
-
+    public class GravityPreviewer : BaseBehaviour {
         public Color Color = Color.green;
         private Vector2 speedModifier = Vector2.one;
         public TileMap TileMap;
         public float TimeIncrementation = 0.1f;
         public Node EndNode;
+        public IGroundEntity Entity;
 
         [Show]
         public Vector2 SpeedModifier {
-            get {
-                return speedModifier;
-            }
-            set {
-                speedModifier = value.normalized;
-            }
-        }
-
-        [Show]
-        public float MaxSpeed {
-            get;
-            private set;
-        }
-
-        [Show]
-        public float JumpHeight {
-            get;
-            private set;
+            get { return speedModifier; }
+            set { speedModifier = value.normalized; }
         }
 
         private void OnDrawGizmosSelected() {
-            if (!TileMap || !RigidBody) {
+            if (!TileMap || Entity == null || !Entity.RigidBody) {
                 return;
             }
-            var speed = new Vector2(MaxSpeed * SpeedModifier.x, JumpHeight * SpeedModifier.y);
-            var path = GravityUtil.CalculatePath(
-                transform.position,
-                speed,
-                TimeIncrementation,
-                TileMap,
-                this,
+            var speed = new Vector2(Entity.MaxSpeed * SpeedModifier.x, Entity.JumpHeight * SpeedModifier.y);
+            var path = GravityUtil.CalculatePath(transform.position, speed, TimeIncrementation, TileMap, Entity,
                 out EndNode);
             Gizmos.color = Color;
             GravityUtil.DrawGizmos(path);
---
+++ Assets/Shiroi/Unity/Pathfinding2D/Util/TestEntity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Vexe.Runtime.Types;
+
+namespace Shiroi.Unity.Pathfinding2D.Util {
+    public class TestEntity : BaseBehaviour, IGroundEntity {
+        public TestEntity() {
+            MaxSpeed = 2f;
+            JumpHeight = 5F;
+        }
+
+        [Show]
+        public Rigidbody2D RigidBody {
+            get;
+            private set;
+        }
+
+        [Show]
+        public float MaxSpeed {
+            get;
+            private set;
+        }
+
+        [Show]
+        public float JumpHeight {
+            get;
+            private set;
+        }
+    }
+}
