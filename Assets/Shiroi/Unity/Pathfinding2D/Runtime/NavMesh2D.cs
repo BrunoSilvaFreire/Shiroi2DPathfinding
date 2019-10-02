0b6e42e2bed7b4ec8a31792a2c16bb2d0eba3ac2
BrunoSilvaFreire <brunorbsf@gmail.com>
2019-10-02 19:10:44 -0300

    > Base classes

--- Assets/Shiroi/Unity/Pathfinding2D/Runtime/NavMesh2D.cs
+++ Assets/Shiroi/Unity/Pathfinding2D/Runtime/NavMesh2D.cs
@@ -1,5 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Shiroi.Unity.Pathfinding2D.Runtime {
-    public class NavMesh2D : MonoBehaviour { }
+    [Serializable]
+    public struct Node {
+        public NodeFlags flags;
+
+        [Flags]
+        public enum NodeFlags {
+            Solid = 1 << 0,
+            LeftWall = 1 << 1,
+            RightWall = 1 << 2,
+            LeftEdge = 1 << 3,
+            RightEdge = 1 << 4
+        }
+    }
+
+    public class NavMesh2D : MonoBehaviour {
+        [SerializeField, HideInInspector]
+        private Node[] nodes;
+
+        public Grid grid;
+        public Vector2Int min, max;
+    }
 }
